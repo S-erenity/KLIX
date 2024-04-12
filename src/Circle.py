21a3c5612a443fd92a62e7a879b4eb09ca38c31c
George Huang <142948455+S-erenity@users.noreply.github.com>
2024-04-12 10:44:45 -0600

    Update Circle.py

--- src/Circle.py
+++ src/Circle.py
@@ -1,1 +1,16 @@
+class Circle:
+    def __init__(self, x, y, diameter, speed, color):
+        self.x = x
+        self.y = y
+        self.diameter = diameter
+        self.speed = speed
+        self.color = color
+
+    def move(self):
+        self.y += self.speed
+
+    def reset(self, width, height):
+        if self.y > height:
+            self.y = random(-500, -50)
+            self.x = random(width)
 
